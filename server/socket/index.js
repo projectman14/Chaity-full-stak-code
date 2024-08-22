738e2a78653a0eec2b214176b38e83ed02faf38d
Lakshya Jain <lakshyajain.14022@gmail.com>
2024-08-22 21:51:09 +0530

    updated index.js for test

--- server/socket/index.js
+++ server/socket/index.js
@@ -14,7 +14,7 @@ const app = express()
 const server = createServer(app)
 const io = new Server(server, {
     cors: {
-        origin: process.env.FRONTEND_URL,
+        origin: `*`,
         credentials: true
     }
 })
